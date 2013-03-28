fe8f4be17fb1ffc8065b2e92cdb55aa6025d7443
Fatemeh Seifipour <Fa.seifipour@gmail.com>
2013-03-28 01:15:04 +0000

    ErrorHandling
    simple example about errorhandling with TDD

---
+++ ErrorHandling/ExceptionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ExceptionTest
+    {
+        [TestMethod]
+        public void If_NameOfUser_Is_Equal_Salam_Return_Exception()
+        {
+            var exception=new RaiseException();
+            try
+            {
+                exception.NameOfUser("Salam");  
+            }
+            catch (Exception ex)
+            {
+
+                Assert.AreEqual(ex.Message, "Salam is not name");
+            }
+            
+        }
+    }
+}
---
+++ ErrorHandling/RaiseException.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+
+namespace ClassLibrary1
+{
+    public class RaiseException
+    {
+        public void NameOfUser(string name)
+        {
+            if(name=="Salam")
+            {
+                throw new ArgumentException("Salam is not name");
+            }
+        }
+       public string MultipleCatches()
+       {
+           string exceptionMessage = string.Empty;
+           try
+           {
+               NameOfUser("Salam");
+           }
+           catch (DivideByZeroException exception)
+           {
+               
+              exceptionMessage="Divided by Zero";
+           }
+           catch(ArgumentException exception)
+           {
+               exceptionMessage = "Argument Exception";
+           }
+           catch(Exception exception)
+           {
+               exceptionMessage = "General Exception";
+           }
+           finally
+           {
+               exceptionMessage = exceptionMessage + "Finally";
+           }
+           return exceptionMessage;
+       }
+        public void Innercatchblock()
+        {
+            try
+            {
+                MultipleCatches();
+            }
+            catch (Exception)
+            {
+                
+                throw new Exception();
+            }
+        }
+    }
+    [Serializable]
+    public class MyException : Exception
+    {
+        public MyException() { }
+        public MyException(string message) : base(message) { }
+        public MyException(string message, Exception inner) : base(message, inner) { }
+        protected MyException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
